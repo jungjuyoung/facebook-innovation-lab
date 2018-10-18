4cacb7ec852a49b43cedec24797ec73db42e844f
jungjuyoung <jungjuyoung02@gmail.com>
2018-10-18 21:13:03 +0900

    템플릿 작업완료

---
+++ ex/function.js
@@ -0,0 +1,5 @@
+// function
+let result = (a = 15, b = 5) => {
+    return (a + b) * 2;
+}
+console.log(result());
--- main.js
+++ main.js
@@ -1,14 +1,42 @@
 const http = require('http');// nodejs에 기본적으로 가지고 있는 기능 중에서 http 모듈을 가져와;
 const url = require('url');// nodejs에 기본적으로 가지고있는 url 모듈을 가져와;
 const fs = require('fs');// nodejs에 기본적으로 가지고 있는 fs파일 시스템 모듈 가져와;
+function templateList(){
+    const topics = fs.readdirSync('data');
+    let listTags = '';
+    console.log(typeof topics, topics)
+    for(let i = 0; i < topics.length; i++){
+        listTags = listTags +`<li><a href="/?id=${topics[i]}">${topics[i]}</a></li>`;
+    }
+    return listTags;
+}
+
+function tempalteHTML(_listTags, _id, _contents){
+    let constent = `<!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <meta http-equiv="X-UA-Compatible" content="ie=edge">
+            <title>facebook Innovation Lab to Web</title>
+        </head>
+        <body>
+            <h1><a href="/">HTML</a></h1>
+            <ol>
+                ${_listTags}
+            </ol>        
+            <h2>${_id}</h2>
+            ${_contents}
+        </body>
+        </html>`;
+        return constent;
+}
 const server = http.createServer ( // http모듈안에 createServer라는 기능으로 서버를 구현
     function(request, response) {
         if(request.url == '/favicon.ico') {
             return response.writeHead(404);
         }
         const parsed_url = url.parse(request.url, true);
-        let id = '';
-        let contents = '';
         // request.url의 값이 /일때
         // id의 값은 Welcome으로 하고 
         // contents의 값은 hello, web으로 한다.
@@ -21,32 +49,9 @@ const server = http.createServer ( // http모듈안에 createServer라는 기능
             id = parsed_url.query.id;
             contents = fs.readFileSync(`data/${id}`, 'utf8');
         }
-        console.log(`id: ${id}`);
-        console.log(`contents: ${contents}`);
-        let topics = fs.readdirSync('data');
-        console.log(`topics: ${topics}`)
-        let listTags = '';
-        topics.map((a, i, ar) => {
-            listTags += `<li><a href="/?id=${a}">${a}</a></li>`;
-        })
+        const listTags = templateList();
+        const content = tempalteHTML(listTags, id, contents)
 
-        let content = `<!DOCTYPE html>
-        <html lang="en">
-        <head>
-            <meta charset="UTF-8">
-            <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <meta http-equiv="X-UA-Compatible" content="ie=edge">
-            <title>facebook Innovation Lab to Web</title>
-        </head>
-        <body>
-            <h1><a href="/">HTML</a></h1>
-            <ol>
-                ${listTags}
-            </ol>        
-            <h2>${id}</h2>
-            ${contents}
-        </body>
-        </html>`;
         response.write(content);
         response.end();
     }
